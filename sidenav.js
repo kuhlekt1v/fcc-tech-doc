243480963472060337d7de0f6160253238bc0fe6
Cody Sheridan <sheridan.codyj@gmail.com>
2018-07-29 08:36:14 -0400

    Corrected mobile querries.

--- sidenav.js
+++ sidenav.js
@@ -1,11 +1,27 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "300px";
-  document.getElementById("main").style.marginLeft = "300px";
+  if (x.matches) {
+    document.getElementById("mySidenav").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "150px";
+  } else {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+  }
 }
+
+var x = window.matchMedia("(max-width: 450px)")
+openNav(x)
+x.addListener(openNav)
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+// Original
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
+} */
